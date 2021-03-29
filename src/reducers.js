30181c7bb9058abd97a402d95d47db399b2ac13e
eric <rfd3344@gmail.com>
2021-03-29 23:52:50 +1100

    remove unnecessary comments

--- src/reducers.js
+++ src/reducers.js
@@ -3,13 +3,8 @@ import { combineReducers } from 'redux';
 
 import todos from '_reducers/todos';
 
-// lastAction reducer
-// function lastAction(state = null, action) { // eslint-disable-line no-unused-vars
-// 	// logger.log(`lastAction: ${state}`, ', newAction', action);
-// 	return action.type;
-// }
 
 export default combineReducers({
-	// lastAction,
+
 	todos,
 });
--- src/themes/PageTheme.js
+++ src/themes/PageTheme.js
@@ -1,9 +1,6 @@
 import React from 'react';
-// import './page.less';
 
 import Routers from './Routers';
-// import Header from './PageHeader';
-// import Footer from './PageFooter';
 
 export default function PageTemplate() {
 	return (
