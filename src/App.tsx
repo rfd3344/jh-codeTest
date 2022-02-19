a01b093266adbd5957d83c89336375be0e748f02
eric <rfd3344w@gmail.com>
2022-02-20 00:05:06 +1100

    1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { theme } from '@/core/theme';
+import { store } from '@/core/store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* <GlobalSnackbar />
+
+        <Routes /> */}
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App;
